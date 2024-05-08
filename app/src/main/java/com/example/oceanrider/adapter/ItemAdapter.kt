1532853f3007f64c3f9ca33c7845e795e3eb26ee
Marie Le Lamer <lelamer.marie17@gmail.com>
2024-05-08 14:48:37 +0200

    quelques modifications du front

--- app/src/main/java/com/example/oceanrider/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/oceanrider/adapter/ItemAdapter.kt
@@ -5,18 +5,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.example.oceanrider.R
 import com.example.oceanrider.model.SurfResponse
 
-class ItemAdapter(private val context: Context, private var spots: List<SurfResponse.Record>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(private val context: Context, private var spots: List<SurfResponse.Record>) :
+    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val surfBreak: TextView = itemView.findViewById(R.id.textView_surfBreak)
+        val difficulty: TextView = itemView.findViewById(R.id.textView_difficulty)
         val address: TextView = itemView.findViewById(R.id.textView_address)
-        val photoUrl: ImageView = itemView.findViewById(R.id.imageView_photos)
+//        val photoUrl: ImageView = itemView.findViewById(R.id.imageView_photos)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,14 +27,14 @@ class ItemAdapter(private val context: Context, private var spots: List<SurfResp
         val record = spots[position]
         val fields = record.fields
         holder.address.text = fields.address
-        holder.surfBreak.text = fields.surfBreak.joinToString { ", " } ?: ""
+        holder.difficulty.text = "Difficulté : " + fields.difficultyLevel.toString()
 
 
-        if (fields.photos.isNotEmpty()) {
-            Glide.with(context)
-                .load(fields.photos[0].url)
-                .into(holder.photoUrl)
-        }
+//        if (fields.photos.isNotEmpty()) {
+//            Glide.with(context)
+//                .load(fields.photos[0].url)
+//                .into(holder.photoUrl)
+//        }
     }
 
     override fun getItemCount(): Int {
@@ -47,29 +46,5 @@ class ItemAdapter(private val context: Context, private var spots: List<SurfResp
         spots = newSpotsList // Mettez à jour la liste de spots dans votre adaptateur
         notifyDataSetChanged() // Notifiez à l'adaptateur que les données ont changé et que l'affichage doit être rafraîchi
     }
-//    private val context: Context,
-//    private val dataset: List<Spot>
-//) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-//    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-//        val surfBreak: TextView = view.findViewById(R.id.textView_surfBreak)
-//        val address: TextView = view.findViewById(R.id.textView_address)
-//        val photoUrl: ImageView = view.findViewById(R.id.imageView_photos)
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-//        val adapterLayout =
-//            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-//        return ItemViewHolder(adapterLayout)
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return dataset.size
-//    }
-//
-//    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-//        val item = dataset[position]
-////        holder.surfBreak.text = item.fields.surfBreak
-////        holder.address.text = item.address
-////        holder.photoUrl.load(item.photos)
-//    }
+
 }
